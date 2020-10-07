75f1adc452f43dc7f2eb238b9f51e1561d7e8e77
Bernardo Borunda <bborunda@pagosdigitales.com>
2020-10-07 11:06:01 -0600

    Inicial

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const actividad = (tarea, duracion, callback) => {
+    setTimeout(() => {
+        callback(tarea);
+    }, duracion);
+}
+
+actividad("1 levantarse", 5000, (tarea) => {
+    console.log("tarea: " + tarea);
+    actividad("2 tender la cama", 4000, (tarea) => {
+        console.log("tarea: " + tarea);
+        actividad("3 bañarse", 3000, (tarea) => {
+            console.log("tarea: " + tarea);
+            actividad("4 clase paradigmas", 5000, (tarea) => {
+                console.log("tarea: " + tarea);
+                actividad("5 desayunar", 8000, (tarea) => {
+                    console.log("tarea: " + tarea);
+                    actividad("6 jugar video juegos", 10000, (tarea) => {
+                        console.log("tarea: " + tarea);
+                    });
+                });
+            });
+        });
+    });
+});
